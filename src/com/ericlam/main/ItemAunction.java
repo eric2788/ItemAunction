7770a20e0b59055e702fff5ab9b3344d0f9fdb9d
Lam <tsukiko852@gmail.com>
2018-11-21 13:49:33 +0800

    bug fix

--- src/com/ericlam/main/ItemAunction.java
+++ src/com/ericlam/main/ItemAunction.java
@@ -103,7 +103,7 @@ public class ItemAunction extends JavaPlugin {
 
         },100L,Config.getInstance().getConfig().getInt("check-interval") * 60 * 20L);
 
-        Bukkit.getScheduler().runTaskAsynchronously(this, ()->new CheckUpdate().plugin());
+        Bukkit.getScheduler().runTaskAsynchronously(this, ()->new CheckUpdate().run());
     }
 
     @Override
